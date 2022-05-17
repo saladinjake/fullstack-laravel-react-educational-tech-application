55c2ca735f26fc937f1532f0db89d7318921db54
saladinjake <juwavictor@gmail.com>
2022-05-17 20:04:57 +0100

    ch: ui-component: Adds helper classes and dummy data

---
+++ src/helper/roleNavigator.js
@@ -0,0 +1,18 @@
+const roleNavigator = (role) => {
+  let route;
+  switch (role) {
+    case "Admin":
+      route = "/admin";
+      break;
+    case "Instructor":
+      route = "/instructor";
+      break;
+    case "User":
+      route = "/courses/display";
+      break;
+    default:
+      route = "/login";
+  }
+  return route;
+};
+export default roleToRoute;
